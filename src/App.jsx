280a969365688a9fd217a20fc8292b725a881b59
Sailendra-18 <sailendrapatelofficial@gmail.com>
2026-01-12 19:05:25 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState,useEffect,useRef} from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Navbar from "./component/navbar";
+import Navbar from "./component/Navbar";
 import { v4 as uuidv4 } from "uuid";
 
 function App() {
